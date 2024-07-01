4535cc4dc7c2649decfd746220faaea9beb6f027
Maryie <mashenka.zinkevich0245@gmail.com>
2024-07-01 17:09:01 +0300

    code was fixed

--- src/HW/2/CurrentUser.tsx
+++ src/HW/2/CurrentUser.tsx
@@ -1,14 +1,16 @@
+import React from 'react';
 import { UserType } from './HW2';
 
 type CurrentUserPropsType = {
-  user: UserType; // пропиши типизацию
+  user: UserType; 
 };
 
 export const CurrentUser = (props: CurrentUserPropsType) => {
+  const { user } = props;
   return (
-    <li key={props.user.id} id={`hw02-user-${props.user.id}`}>
-      <strong>{props.user.name}</strong> (Age: {props.user.age})<strong> Address: </strong>
-      {props.user.address.street}, {props.user.address.city}
+    <li key={user.id} id={`hw02-user-${user.id}`}>
+      <strong>{user.name}</strong> (Age: {user.age})<strong> Address: </strong>
+      {user.address.street}, {user.address.city}
     </li>
   );
 };
