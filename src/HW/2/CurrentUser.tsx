be5f311b2703cf4c72d6ea5e3cf16afd742f1e46
Maryie <mashenka.zinkevich0245@gmail.com>
2024-07-01 17:32:44 +0300

    code fixed

--- src/HW/2/CurrentUser.tsx
+++ src/HW/2/CurrentUser.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { UserType } from './HW2';
 
 type CurrentUserPropsType = {
-  user: UserType; 
+  user: UserType;
 };
 
 export const CurrentUser = (props: CurrentUserPropsType) => {
   const { user } = props;
   return (
-    <li key={user.id} id={`hw02-user-${user.id}`}>
-      <strong>{user.name}</strong> (Age: {user.age})<strong> Address: </strong>
+    <li id={`hw02-user-${user.id}`}>
+      <strong>{user.name}</strong> (Age: {user.age}) <strong> Address: </strong>
       {user.address.street}, {user.address.city}
     </li>
   );
--- src/HW/2/UserList2.tsx
+++ src/HW/2/UserList2.tsx
@@ -8,14 +8,12 @@ type UserList2PropsType = {
 
 export const UserList2 = (props: UserList2PropsType) => {
   return (
-    <div id={'hw02-users'}>
+    <div id="hw02-users">
       <h2>User List:</h2>
-
-      <button id={'hw02-filter-button'} onClick={props.filterUsers}>SHOW ME FRIENDS FROM LA</button>
-
+      <button id="hw02-filter-button" onClick={props.filterUsers}>SHOW ME FRIENDS FROM LA</button>
       <ul>
         {props.users.myFriends.map((user) => (
-          <CurrentUser key={user.id} user={user} /> 
+          <CurrentUser key={user.id} user={user} />
         ))}
       </ul>
     </div>
